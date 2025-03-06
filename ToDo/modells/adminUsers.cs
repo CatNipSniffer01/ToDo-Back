687847d215e1626211dcabe5d348fd9f8bd97c9f
bp-ter <bertapeter@ktch.hu>
2025-03-06 11:06:22 +0100

    AdminUser Patch és tesztelés

--- ToDo/modells/adminUsers.cs
+++ ToDo/modells/adminUsers.cs
@@ -4,9 +4,9 @@ namespace ToDo.modells{
         [PrimaryKey("AdminUser_Id")]
     public class adminUsers{
         public int AdminUser_Id { get; set; }
-        public int AdminUserName { get; set; }
-        public int AdminUser_Password { get; set; }
-        public int AdminUser_Email { get; set; }
+        public string AdminUserName { get; set; }
+        public string AdminUser_Password { get; set; }
+        public string AdminUser_Email { get; set; }
         public int AdminUserAcc_CR_D {  get; set; }
         public int AdminUserAcc_UP_D { get; set; }
     }
